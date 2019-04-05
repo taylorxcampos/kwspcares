2851e1fa597e452cfa19ede19c821c5ac975747f
Chris LoPresti <lopresti.chris@gmail.com>
2019-04-04 23:29:39 -0400

    Updates

---
+++ precache-manifest.1c5720dd01c7c152262ebbb76e990f7c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "7d8430a50ef8142667b018d8648aea9c",
+    "url": "/static/media/redday.7d8430a5.png"
+  },
+  {
+    "revision": "49b71715174a80d7af97648ca8a475a2",
+    "url": "/static/media/kw-logo.49b71715.png"
+  },
+  {
+    "revision": "cdf885132cf8316338987ecd837bcd71",
+    "url": "/static/media/giphy.cdf88513.gif"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "d960332dbd01f8d972d2",
+    "url": "/static/js/main.cc996081.chunk.js"
+  },
+  {
+    "revision": "1539c79705bb6723e1e6",
+    "url": "/static/js/2.37bfbe86.chunk.js"
+  },
+  {
+    "revision": "d960332dbd01f8d972d2",
+    "url": "/static/css/main.bfce0b54.chunk.css"
+  },
+  {
+    "revision": "1539c79705bb6723e1e6",
+    "url": "/static/css/2.44c34953.chunk.css"
+  },
+  {
+    "revision": "f3aa73a8be6d85e2ce9e049c8e9e97d5",
+    "url": "/index.html"
+  }
+];
